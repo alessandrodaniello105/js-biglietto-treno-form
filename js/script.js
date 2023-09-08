a670ab8e2e2e4cacf5c5b350957b463312d42b45
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-08 17:26:14 +0200

    [add]: form/ticket reset

--- js/script.js
+++ js/script.js
@@ -5,12 +5,20 @@ const seniorDiscount = 40;
 
 
 const buttonCreate = document.getElementById('button-create');
+const buttonReset = document.getElementById('button-reset')
+const ticketPow = document.querySelector('.ticket-one');
 
 buttonCreate.addEventListener('click', ticketCreation);
+buttonReset.addEventListener('click', ticketReset)
+
+let carriage, message, ticketCost, ticketCostVerify;
+
+
 
 function ticketCreation(){
   const nameUser = document.getElementById('name-user').value;
   const kmTrip = parseInt(document.getElementById('km-trip').value);
+  carriage = Math.ceil(Math.random() * 10);
 
   let ticketCost = kmTrip * priceKm;
 
@@ -20,9 +28,15 @@ function ticketCreation(){
     ticketCost *= 1 - (juniorDiscount / 100);
   }
   
+  ticketPow.classList.remove('d-none')
+  
   document.getElementById('outputName').innerHTML = nameUser;
   document.getElementById('outputPrice').innerHTML = ticketCost;
+  document.getElementById('outputCarriage').innerHTML = carriage;
+
 }
 
-let message, ticketCost, ticketCostVerify;
 
+function ticketReset(){
+  ticketPow.classList.add('d-none')
+}
