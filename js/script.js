b5bd3cee1dea08818a15415db023117345a4bad3
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-08 17:00:01 +0200

    [del]: form tag; [add]: discount ticket cost calc

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const priceKm = 0.21;
+const ageUser = document.getElementById('age-user');
+const juniorDiscount = 20;
+const seniorDiscount = 40;
+
 
 const buttonCreate = document.getElementById('button-create');
 
@@ -8,11 +12,17 @@ function ticketCreation(){
   const nameUser = document.getElementById('name-user').value;
   const kmTrip = parseInt(document.getElementById('km-trip').value);
 
-  ticketCost = priceKm * kmTrip;
+  let ticketCost = kmTrip * priceKm;
+
+  if (ageUser.value == 3) {
+    ticketCost *= 1 - (seniorDiscount / 100);
+  } else if (ageUser.value == 1) {
+    ticketCost *= 1 - (juniorDiscount / 100);
+  }
   
   document.getElementById('outputName').innerHTML = nameUser;
   document.getElementById('outputPrice').innerHTML = ticketCost;
 }
 
-let message, ticketCost;
+let message, ticketCost, ticketCostVerify;
 
