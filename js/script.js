2960d2f34508598603c90eec01e951a195733014
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-08 16:39:24 +0200

    [add]: base ticketCost output

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const kmTrip = parseInt(document.getElementById('km-trip').value);
 const priceKm = 0.21;
 
 const buttonCreate = document.getElementById('button-create');
@@ -7,6 +6,13 @@ buttonCreate.addEventListener('click', ticketCreation);
 
 function ticketCreation(){
   const nameUser = document.getElementById('name-user').value;
+  const kmTrip = parseInt(document.getElementById('km-trip').value);
+
+  ticketCost = priceKm * kmTrip;
   
-  document.getElementById('outputName').innerHTML = nameUser
+  document.getElementById('outputName').innerHTML = nameUser;
+  document.getElementById('outputPrice').innerHTML = ticketCost;
 }
+
+let message, ticketCost;
+
