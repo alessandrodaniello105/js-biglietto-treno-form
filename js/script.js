58f9d7834b538ef79bf4465f2d957de72571880a
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-08 16:29:33 +0200

    [add]: problem const, function 'ticketCreation'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const kmTrip = parseInt(document.getElementById('km-trip').value);
+const priceKm = 0.21;
+
+const buttonCreate = document.getElementById('button-create');
+
+buttonCreate.addEventListener('click', ticketCreation);
+
+function ticketCreation(){
+  const nameUser = document.getElementById('name-user').value;
+  
+  document.getElementById('outputName').innerHTML = nameUser
+}
