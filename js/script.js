10d7d097c7c65d25c8f5619322277538c07aef0f
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-11 13:15:18 +0200

    [add]: comments/offerta(+output)/parseFloat

--- js/script.js
+++ js/script.js
@@ -5,51 +5,62 @@ const seniorDiscount = 40;
 
 //BUTTONS
 const buttonCreate = document.getElementById('button-create');
-const buttonReset = document.getElementById('button-reset')
+const buttonReset = document.getElementById('button-reset');
 
 buttonCreate.addEventListener('click', ticketCreation);
-buttonReset.addEventListener('click', ticketReset)
+buttonReset.addEventListener('click', ticketReset);
 
 //OUTPUT
 const outputName = document.getElementById('outputName');
 const outputPrice = document.getElementById('outputPrice');
 const outputCarriage = document.getElementById('outputCarriage');
 const outputNumberTicket = document.getElementById('outputNumberTicket');
+const outputOfferta = document.getElementById('outputOfferta');
 
 
 const ticketPow = document.querySelector('.ticket-one');
 
-let carriage, message, ticketCost, ticketCostVerify;
+let carriage, ticketCost, ticketCostVerify;
 
 
 function ticketCreation(){
+
   const nameUser = document.getElementById('name-user').value;
-  const kmTrip = parseInt(document.getElementById('km-trip').value);
+  const kmTrip = parseFloat(document.getElementById('km-trip').value);
 
   carriage = Math.ceil(Math.random() * 10); //random carriage generator
   numberTicket = Math.ceil(Math.random() * 10000) + 90000; // random ticket number generator
 
   let ticketCost = kmTrip * priceKm;
+  let offerta = 'Biglietto Standard';
 
   if (ageUser.value == 3) {
+
+    offerta = 'Sconto Over65';
     ticketCost *= 1 - (seniorDiscount / 100);
+
   } else if (ageUser.value == 1) {
+
+    offerta = 'Sconto Minorenni';
     ticketCost *= 1 - (juniorDiscount / 100);
+
   }
   
-  ticketPow.classList.remove('d-none')
+  ticketPow.classList.remove('d-none');
   
   outputName.innerHTML = nameUser;
-  outputPrice.innerHTML = ticketCost;
+  outputPrice.innerHTML = ticketCost.toFixed(2) + ' €';
   outputCarriage.innerHTML = carriage;
   outputNumberTicket.innerHTML = numberTicket;
+  outputOfferta.innerHTML = offerta;
 
   document.getElementById('buy-btn').innerHTML = `
   <button class="btn btn-info">Acquista Biglietto</button>
   `;
+
 }
 
 
 function ticketReset(){
-  ticketPow.classList.add('d-none')
+  ticketPow.classList.add('d-none');
 }
